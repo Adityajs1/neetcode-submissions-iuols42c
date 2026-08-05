c4a95b104ac5af74d3d742b58a4a96f747987605
Aditya Tiwari <aditya96937@gmail.com>
2026-08-05 10:46:48 +0530

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+
+        if (root == null)
+            return "#";
+
+        StringBuilder str = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            TreeNode node = queue.poll();
+
+            if (node == null) {
+                str.append("#,");
+                continue;
+            }
+
+            str.append(node.val).append(",");
+
+            queue.offer(node.left);
+            queue.offer(node.right);
+        }
+
+        return str.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+        if (data.equals("#"))
+            return null;
+
+        String[] values = data.split(",");
+
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int i = 1;
+
+        while (!queue.isEmpty()) {
+
+            TreeNode parent = queue.poll();
+
+            // Left child
+            if (!values[i].equals("#")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                queue.offer(left);
+            }
+            i++;
+
+            // Right child
+            if (!values[i].equals("#")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                queue.offer(right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
